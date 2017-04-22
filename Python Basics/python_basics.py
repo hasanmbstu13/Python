31210577fac719b383af5cec8f9878f6350433e6
hasanmbstu13 <hasanmbstu13@gmail.com>
2017-04-22 15:07:19 +0600

    Pracitse input/output and str formatting in python

---
+++ Python Basics/python_basics.py
@@ -0,0 +1,38 @@
+print("hello") # print a sequence of arguments for user to read.
+
+print("hello", "there")
+
+
+
+def square_return(num):
+    return num ** 2;
+
+def square_print(num):
+    print("The square of num is", num**2)
+
+# During execution of a function call,
+# if the end of the function body is reached without executing a return statement,
+# that function call produces value none.
+
+def add(num1, num2):
+	print(num1+num2)
+
+result = add(1,3)
+# new_result = result + 1; #that will produce typeError because of adding with none
+
+def add(number1, number2):
+    return number1 + number2
+    print("hello")
+
+result = add(1, 3);
+
+#name = input("What is your name? ");
+#location = input("What is your location? ")
+#print(name, "lives in", location)
+
+#age = input("How old are you? ") # Regardless of the text entered by the user, the input function returns str
+
+print('''how
+are
+you?''')
+             
