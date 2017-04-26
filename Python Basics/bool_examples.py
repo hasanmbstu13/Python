065d174808eeb4c2ab5c21e9f49e105c203f7e0c
hasanmbstu13 <hasanmbstu13@gmail.com>
2017-04-26 22:35:17 +0600

    Checking a number is even or not

---
+++ Python Basics/bool_examples.py
@@ -0,0 +1,15 @@
+def is_even(num):
+    ''' (int) -> bool
+
+    Return whether num is even.
+    
+    >>>is_even(4)
+    True
+    >>>is_even(77)
+    False
+    '''
+
+    if num % 2 == 0:
+        return True
+    else:
+        return False
