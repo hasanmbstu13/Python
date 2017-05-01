1ed93d9ef11dcdde83f703f84bfb75a4b45663e6
hasanmbstu13 <hasanmbstu13@gmail.com>
2017-05-01 16:29:26 +0600

    Learn mutability and aliases

--- Python Basics/list_methods.py
+++ Python Basics/list_methods.py
@@ -36,3 +36,51 @@ list2.append(4)
 
 # Answer will 4
 # list1 and list2 refer to the same list object, so changes to that list will be seen by both variables.
+
+def double_even_indices(lst):
+    """ (list of int) -> NoneType
+
+    Double every other int in lst, starting at index 0.
+    """
+
+    i = 0
+    while i < len(lst):
+        lst[i] = lst[i] * 2
+        i = i + 2
+
+    list1 = [11, 12, 13, 14, 15, 16, 17]
+    print(list1)
+    double_even_indices(list1)
+    print(list1)
+
+
+def double_first_element(lst):
+    if len(lst) > 0:
+       lst[0] = lst[0] * 2
+
+list1 = [10, 100, 1000]
+double_first_element(list1)
+
+# Answer will 20
+# lst and list1 refer to the same list object, so the statement
+# lst[0] = lst[0]*2 will change the list that list1 refers to.
+
+def interrupted(s):
+    s[-1] = "-"
+
+greeting = "how are you"
+interrupted(greeting)
+print(greeting)
+
+# Python raises an error
+# Any attempt to modify an immutable type will raise a TypeError
+
+lst1 = [11, 12, 13, 14, 15, 16, 17]
+lst2 = lst1
+lst1[-1] = 18
+lst2
+
+# After the second statement executes, lst1 and lst2
+# both refer to the same list. When two variables
+# refer to the same objects, they are aliases. If that list is modified,
+# both of lst1 and lst2 will see the change.
