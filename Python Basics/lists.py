c2572c2f58c550121679863e6d4796ebff923a3a
hasanmbstu13 <hasanmbstu13@gmail.com>
2017-05-01 11:52:41 +0600

    Understanding the lists of python

---
+++ Python Basics/lists.py
@@ -0,0 +1,22 @@
+grades = [80, 90, 70] 
+grades[0]
+grades[1]
+grades[1:2]
+80 in grades
+60 in grades
+len(grades)
+min(grades)
+
+subjects = ['bio', 'cs', 'math', 'history']
+len(subjets)
+max(subjects)
+sum(subjects) #return will error
+street_address = [10, 'Main Street']
+
+
+for grade in grades:
+    print(grade)
+
+
+for item in subjects:
+    print(item)
