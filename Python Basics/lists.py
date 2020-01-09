2793d4a8589b9a19610a6671e41ee4995fbf395f
Mainul Hasan <mainul@mvisolutions.com>
2020-01-09 15:26:07 +0600

    Add comment to make more list object readable

--- Python Basics/lists.py
+++ Python Basics/lists.py
@@ -1,6 +1,8 @@
 grades = [80, 90, 70] 
 grades[0]
 grades[1]
+print(grades[-2]) # will print 90 count from the last index of the lists, the last index of the lists is -1
+# Slicing a list
 grades[1:2]
 80 in grades
 60 in grades
@@ -21,6 +23,9 @@ for grade in grades:
 for item in subjects:
     print(item)
 
+
+# Lists can contain any type of data, including integers, strings, and even other lists.
+# A list is usually used to contain items of the same kind, like temperatures or dates or grades in a course.
 
 
 
