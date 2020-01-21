7c5ab45b3cd28f4c664100db8dccdf652130c9b4
mainul <mainul@mvisolutions.com>
2020-01-21 14:51:03 +0600

    Add organizer apps model

--- Django/projects/first_project/organizer/models.py
+++ Django/projects/first_project/organizer/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=31)
+    slug = models.CharField(max_length=31)
+
+class Startup(models.Model):
+    name = models.CharField(max_length=31)
+    slug = models.SlugField(max_length=31)
+    description = models.TextField()
+    founded_date = models.DateField()
+    contact = models.EmailField()
+    website = models.URLField()
+
+class NewsLink(models.Model):
+    title = models.CharField(max_length=31)
+    slug = models.CharField(max_length=31)
+    pub_date = models.DateField()
+    link = models.URLField()
