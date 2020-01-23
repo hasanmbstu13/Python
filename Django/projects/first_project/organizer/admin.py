5bcdeb8499a9eb52bc5f22e6063c25004370b043
Mainul Hasan <mainul@mvisolutions.com>
2020-01-23 14:39:29 +0600

    Customize django admin

--- Django/projects/first_project/organizer/admin.py
+++ Django/projects/first_project/organizer/admin.py
@@ -3,5 +3,16 @@ from django.contrib import admin
 from .models import NewsLink, Startup, Tag
 
 admin.site.register(NewsLink)
-admin.site.register(Startup)
-admin.site.register(Tag)
+
+# Python Decorator
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+
+    list_display = ("name", "slug")
+
+
+@admin.register(Startup)
+class StartupAdmin(admin.ModelAdmin):
+
+    list_display = ("name", "slug")
+    prepopulated_fields = {"slug": ("name", )}
