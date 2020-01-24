5f7208cae7d9e939c2791285fe53c76ee1146f86
Mainul Hasan <mainul@mvisolutions.com>
2020-01-24 17:20:31 +0600

    Make serialize class for organizer and blog

--- Django/projects/first_project/organizer/serializers.py
+++ Django/projects/first_project/organizer/serializers.py
@@ -1,40 +1,37 @@
-from rest_framework.fields import (
-    CharField,
-    DateField,
-    EmailField,
-    IntegerField,
-    SlugField,
-    URLField
-)
+"""Serializers for the Organizer App
 
-from rest_framework.serializers import Serializer
+Serializer Documentation
+http://www.django-rest-framework.org/api-guide/serializers/
+http://www.django-rest-framework.org/api-guide/fields/
+http://www.django-rest-framework.org/api-guide/relations/
+"""
 
-class TagSerializer(Serializer):
+from rest_framework.serializers import ModelSerializer
+
+from .models import NewsLink, Startup, Tag
+
+class TagSerializer(ModelSerializer):
     """Serialize Tag data"""
 
-    id = IntegerField(read_only=True)
-    name = CharField(max_length=31)
-    slug = SlugField(max_length=31, allow_blank=True)
+    class Meta:
+        model = Tag
+        fields = "__all__"
 
-class StartupSerializer(Serializer):
+class StartupSerializer(ModelSerializer):
     """Serialize Startup data"""
 
-    id = IntegerField(read_only=True)
-    name = CharField(max_length=31)
-    slug = SlugField(max_length=31)
-    description = CharField()
-    founded_date = DateField()
-    contact = EmailField()
-    website = URLField(max_length=255)
     tags = TagSerializer(many=True)
 
-class NewsLinkSerializer(Serializer):
+    class Meta:
+        model = Startup
+        fields = "__all__"
+
+class NewsLinkSerializer(ModelSerializer):
     """Serialize NewsLink data"""
 
-    id = IntegerField(read_only=True)
-    title = CharField(max_length=63)
-    slug = SlugField(max_length=63)
-    pub_date = DateField()
-    link = URLField(max_length=255)
     startup = StartupSerializer()
+
+    class Meta:
+        model = NewsLink
+        fields = "__all__"
 
