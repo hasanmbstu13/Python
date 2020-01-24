5f823ddfafc2c47eb1ae03993172d4d6c6916a0f
Mainul Hasan <mainul@mvisolutions.com>
2020-01-24 14:48:33 +0600

    Use utility functions to organize code

--- Django/projects/first_project/organizer/views.py
+++ Django/projects/first_project/organizer/views.py
@@ -1,41 +1,50 @@
 import json
 
 from django.http import Http404, HttpResponse
+
+from django.shortcuts import (
+    get_list_or_404,
+    get_object_or_404,
+)
+
 from django.views import View
 
 from .models import Tag
 
+def serialize_tag_to_dict(tag):
+    return dict(
+        id=tag.pk,
+        name=tag.name,
+        slug=tag.slug,
+    )
+
+def serialize_tag_to_json(tag):
+    return json.dumps(serialize_tag_to_dict(tag))
+
+def serialize_tag_list_to_json(tag_list):
+    return json.dumps(
+        list(map(serialize_tag_to_dict, tag_list))
+    )
+
 class TagApiDetail(View):
 
     def get(self, request, pk):
-        try:
-            tag = Tag.objects.get(pk=pk)
-        except Tag.DoesNotExist:
-            raise Http404()
-        tag_json = json.dumps(
-            dict(
-                id=tag.pk,
-                name=tag.name,
-                slug=tag.slug,
-            )
+        tag = get_object_or_404(Tag, pk=pk)
+        tag_json = serialize_tag_to_json(tag)
+        return HttpResponse(
+            tag_json,
+            content_type="application/json"
         )
-        return HttpResponse(tag_json)
 
 
 class TagApiList(View):
 
     def get(self, request):
-        tag_list = Tag.objects.all()
+        tag_list = get_list_or_404(Tag)
         if not tag_list:
             raise Http404()
-        tag_json = json.dumps(
-            [
-                dict(
-                    id=tag.pk,
-                    name=tag.name,
-                    slug=tag.slug,
-                )
-                for tag in tag_list
-            ]
+        tag_json = serialize_tag_list_to_json(tag_list)
+        return HttpResponse(
+            tag_json,
+            content_type="application/json"
         )
-        return HttpResponse(tag_json)
