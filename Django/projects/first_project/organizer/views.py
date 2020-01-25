1c6d66fef45bb6ae5e18f9acf443736a00c88dd6
Mainul Hasan <mainul.hasan.cse@gmail.com>
2020-01-25 12:20:57 +0600

    Integrate serializers with views

--- Django/projects/first_project/organizer/views.py
+++ Django/projects/first_project/organizer/views.py
@@ -1,42 +1,37 @@
 """Views for Organizer App"""
-import json
-
-from django.http import Http404, HttpResponse
-
 from django.shortcuts import (
     get_list_or_404,
     get_object_or_404,
 )
 
-from django.views import View
-from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .serializers import TagSerializer
 from .models import Tag
 
-class TagApiDetail(View):
+class TagApiDetail(APIView):
     """Return JSON for single Tag object"""
 
     def get(self, request, pk):
         """Handle GET HTTP method"""
         tag = get_object_or_404(Tag, pk=pk)
-        s_tag = TagSerializer(tag)
-        tag_json = JSONRenderer().render(s_tag.data)
-        return HttpResponse(
-            tag_json,
-            content_type="application/json"
+        s_tag = TagSerializer(
+            tag,
+            context={"request": request},
         )
+        return Response(s_tag.data)
 
 
-class TagApiList(View):
+class TagApiList(APIView):
     """Return JSON for multiple Tag objects"""
 
     def get(self, request):
         """Handle GET HTTP method"""
         tag_list = get_list_or_404(Tag)
-        s_tag = TagSerializer(tag_list, many=True)
-        tag_json = JSONRenderer().render(s_tag.data)
-        return HttpResponse(
-            tag_json,
-            content_type="application/json"
+        s_tag = TagSerializer(
+            tag_list,
+            many=True,
+            context={"request": request},
         )
+        return Response(s_tag.data)
