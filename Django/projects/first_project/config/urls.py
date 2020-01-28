a89d8db3d3a0b52b186975c22df7d10ad413676b
Mainul Hasan <mainul@mvisolutions.com>
2020-01-28 15:55:19 +0600

    Update URL for api

--- Django/projects/first_project/config/urls.py
+++ Django/projects/first_project/config/urls.py
@@ -17,11 +17,12 @@ Root URL Configuration for Startup Organizer Project
 from django.contrib import admin
 from django.urls import path, include
 
-from blog import urls as blog_urls
-from organizer import urls as organizer_urls
+from blog.routers import urlpatterns as blog_api_urls
+from organizer.routers import urlpatterns as organizer_api_urls
+
+api_urls = blog_api_urls + organizer_api_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include(organizer_urls)),
-    path("", include(blog_urls)),
+    path("api/v1/", include(api_urls)),
 ]
